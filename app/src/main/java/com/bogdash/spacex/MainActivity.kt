c219f8696db3154789670b739fe53bf20511febe
Anastasia <ms.anastasia.bogdanova@gmail.com>
2024-04-14 22:46:49 +0300

    Add viewPager

--- app/src/main/java/com/bogdash/spacex/MainActivity.kt
+++ app/src/main/java/com/bogdash/spacex/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.bogdash.spacex.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -15,15 +16,32 @@ class MainActivity : AppCompatActivity() {
         LaunchesFragment.newInstance(),
         RocketsFragment.newInstance()
     )
+
+    private val titles = listOf(
+        "Upcoming", "Launches", "Rockets"
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        initViewPager()
+    }
+
+    private fun initViewPager() {
+        with(binding) {
+            val viewPagerAdapter = ViewPagerAdapter(this@MainActivity, listFragment)
+            viewPager.adapter = viewPagerAdapter
+            TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+                tab.text = titles[position]
+            }.attach()
         }
     }
 }
